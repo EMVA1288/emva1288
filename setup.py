2b991aab2e83ea3283af5ba1a8938fb6d92ecdf6
Federico Ariza <ariza.federico@gmail.com>
2015-07-07 22:27:53 -0400

    adapting rst text

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 from setuptools import setup
+
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst')) as f:
+    long_description = f.read()
+
 setup(name='emva1288',
       packages=['emva1288'],
-      version='0.1.1',
+      version='0.1.2',
       description='EMVA1288 reference implementation',
+      long_description=long_description,
       author='Federico Ariza',
       author_email='ariza.federico@gmail.com',
       url='https://github.com/EMVA1288/emva1288',
       keywords=['sensors', 'cameras'],
-      classifiers=[],
+      classifiers=['Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.4'],
       install_requires=['matplotlib',
                         'numpy',
                         'pillow',
