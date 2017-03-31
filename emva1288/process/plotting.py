fix stack units in fft

emva1288/process/plotting.py
@@ -391,11 +391,11 @@ class PlotHorizontalSpectrogramPRNU(Emva1288Plot):
 
         ax.axhline(test.PRNU1288,
                    label='$PRNU_{1288}$',
-                   linestyle='-.',
-                   color='b',
+                   linestyle='--',
+                   color='r',
                    gid='%d:marker' % test.id)
 
-        ax.axhline(np.sqrt(test.sigma_2_y_stack),
+        ax.axhline(100 * np.sqrt(test.sigma_2_y_stack) / data_mean,
                    label='$\sigma^2_{y.stack}$',
                    linestyle='-.',
                    color='g',
@@ -461,7 +461,7 @@ class PlotVerticalSpectrogramPRNU(Emva1288Plot):
                    color='r',
                    gid='%d:marker' % test.id)
 
-        ax.axhline(np.sqrt(test.sigma_2_y_stack) / data_mean,
+        ax.axhline(100 * np.sqrt(test.sigma_2_y_stack) / data_mean,
                    label='$\sigma^2_{y.stack}$',
                    linestyle='-.',
                    color='g',

emva1288/process/routines.py
@@ -13,6 +13,7 @@ from scipy.optimize import leastsq
 from lxml import etree
 from PIL import Image
 from collections import OrderedDict
+# import cv2
 
 
 SIGNIFICANT_DIGITS = 7
@@ -21,8 +22,8 @@ SIGNIFICANT_DIGITS = 7
 def load_image(fname):
     img = Image.open(fname)
     img = np.asarray(img.split()[0])
-#     img = cv2.imread(fname, cv2.CV_LOAD_IMAGE_UNCHANGED)
-#     img = cv2.split(img)[0]
+    # img = cv2.imread(fname, cv2.CV_LOAD_IMAGE_UNCHANGED)
+    # img = cv2.split(img)[0]
     return img
 
 