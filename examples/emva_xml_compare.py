c415450387b75cf3af6f5d8486161c0cd72beeca
Wütschner Manfred [STEMMER IMAGING AG] <mwuetschner@de.stemmer.local>
2018-12-10 15:18:48 +0100

    added new samples for dataset processing

---
+++ examples/emva_xml_compare.py
@@ -0,0 +1,11 @@
+import os
+from emva1288 import process
+
+#specify the path to the files to compare
+dir_ = '/home/work/1288/datasets/EMVA1288_ReferenceSet_001_CCD_12Bit/'
+fresult1 = 'EMVA1288_Result1.xml'
+fresult2 = 'EMVA1288_Result2.xml'
+fcompare = 'EMVA1288_Compare.txt'
+
+u = process.routines.compare_xml(os.path.join(dir_, fresult1), os.path.join(dir_, fresult2), os.path.join(dir_, fcompare));
+print(u);
---
+++ examples/emva_xml_create.py
@@ -0,0 +1,21 @@
+import os
+from emva1288 import process
+
+#specify the path to the image stack
+dir_ = '/home/work/1288/datasets/EMVA1288_ReferenceSet_001_CCD_12Bit/'
+fname = 'EMVA1288_Data.txt'
+fname = os.path.join(dir_, fname)
+fresult = 'EMVA1288_Result.xml'
+fresult = os.path.join(dir_, fresult)
+
+parser = process.ParseEmvaDescriptorFile(os.path.join(dir_, fname))
+imgs = process.LoadImageData(parser.images)
+dat = process.Data1288(imgs.data)
+
+res = process.Results1288(dat.data)
+
+res.print_results()
+
+f =  open(os.path.join(dir_, fresult), "wb")
+f.write(res.xml())
+f.close()
